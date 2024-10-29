67f4362157758fe73ccc3123b6001b4189b96a4a
Vinul Vinsura <vinulviksura16@gmail.com>
2024-10-29 13:11:11 +0530

    project initiation

---
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.example.productservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+@CrossOrigin
+public class ProductController {
+}
